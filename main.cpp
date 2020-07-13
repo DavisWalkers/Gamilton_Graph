84ba25f98296d36f2ee35d11b2cdd4f961e3c416
DavisWalkers <39212741+DavisWalkers@users.noreply.github.com>
2020-07-13 15:38:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,59 +6,11 @@ using namespace std;
 
 int main()
 {            
-    /* Testing function correctness */
-    int size = 5;
-    int **graph = new int*[size];
-
-    for(int i = 0; i < size; ++i)
-        graph[i] = new int[size];
-
-    /* Manually fill in the graph */
-    /* Test 1*/
-    /*
-        | 0 1 0 0 1 |
-        | 1 0 1 1 0 |
-        | 0 1 0 1 1 |
-		| 0 1 1 0 0 |
-		| 1 0 1 0 0 |
-    */
-    graph[0][0] = 0, graph[0][1] = 1, graph[0][2] = 1, graph[0][3] = 0, graph[0][4] = 0;
-    graph[1][0] = 1, graph[1][1] = 0, graph[1][2] = 1, graph[1][3] = 1, graph[1][4] = 0;
-    graph[2][0] = 1, graph[2][1] = 1, graph[2][2] = 0, graph[2][3] = 0, graph[2][4] = 1;
-    graph[3][0] = 0, graph[3][1] = 1, graph[3][2] = 0, graph[3][3] = 0, graph[3][4] = 1;
-    graph[4][0] = 0, graph[4][1] = 0, graph[4][2] = 1, graph[4][3] = 1, graph[4][4] = 0;
-    
-    /* Calculating result */
-    bool result = isGamilton(graph, size);
-    bool expected = true;
-
-    cout << "Test 1: " << ((result == expected) ? "Passed" : "Not Passed") << endl;
-
-    /* Test 2 */
-    /*
-        | 0 0 1 |
-        | 0 0 1 |
-        | 1 1 0 |
-    */
-	deleteGraph(graph, size);
-	size = 3;	
-	graph = new int*[size];
-	for (int i = 0; i < size; ++i)
-		graph[i] = new int[size];
-
-    graph[0][0] = 0, graph[0][1] = 0, graph[0][2] = 1;
-    graph[1][0] = 0, graph[1][1] = 0, graph[1][2] = 1;
-    graph[2][0] = 1, graph[2][1] = 1, graph[2][2] = 0;
-
-    /* Calculating result */
-    result = isGamilton(graph, size);
-    expected = false;
-
-    cout << "Test 2: " << ((result == expected) ? "Passed" : "Not Passed") << endl;
-	deleteGraph(graph, size);
+    /* Test for correctness */
+    testCorrectness();
 
     /* Measuring time on arbitrary graphs */
-	test();
+	//test();
 
     return 0;
 }
